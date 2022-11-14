e92d86919e7f1f7582eb0a662da147f915d4f766
Cassandra CHAUMULON <cassandra1d@hotmail.fr>
2022-11-14 21:29:20 +0100

    modifs

--- train.py
+++ train.py
@@ -7,36 +7,48 @@ import torch.optim as optim
 import torch.utils.data.dataloader as dataloader
 from torch.autograd import Variable
 
-
 from dataloader import m2set
 from tcn import TCN
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
-name = "results_" + str(datetime.datetime.now()) + ".txt"
-f = open(name, "w")
-f.write(
-    "Parameters : batch_size = 32, lr = 0.01, weight_decay = 0.0001, momentum = 0.9, TCN layer size = [1,2,3,2], "
-    "dropout = 0.2, kernel_size = 2 \n")
+def append_new_line(file_name, text_to_append):
+    """Append given text as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+") as file_object:
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # If file is not empty then append '\n'
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        # Append text at the end of file
+        file_object.write(text_to_append)
 
-batch_size=32
+
+batch_size = 32
+learning_rate = 0.01
+weight_decay = 0.0001
+momentum = 0.9
+num_epochs = 15
+
+name = "results_" + str(datetime.datetime.now()) + ".txt"
+append_new_line(name,
+        "Parameters : batch_size = " + str(batch_size) + " , lr = " + str(learning_rate) + ", weight_decay = " + str(
+            weight_decay) + ", momentum = " + str(momentum) + ", TCN layer size = [1,2,3,2], "
+                                                              "optimizer = SGD, num_epochs = " + str(num_epochs))
 
 tcn_model = TCN(80, [1, 2, 3, 2])
 m2s = m2set(cutset_file="lists/allies_fbank_vad.jsonl.gz")
 train = m2s
 train, valid = torch.utils.data.random_split(train, [2784, 492])
+
 dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=2, pin_memory=True)
 train_loader = dataloader.DataLoader(train, **dataloader_args)
 valid_loader = dataloader.DataLoader(valid, **dataloader_args)
-learning_rate = 0.01
-weight_decay = 0.0001
-momentum = 0.9
-
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # tcn_optimizer = optim.Adam(tcn_model.parameters(), lr=learning_rate)
 tcn_optimizer = optim.SGD(tcn_model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
-num_epochs = 15
+
 loss = 1
 lossTmp = 0
 cpt = 0
@@ -51,6 +63,7 @@ while cpt != 3 and epoch < num_epochs:
     tcn_losses = []
     correct = 0
 
+    print("Start of train")
     for batch_idx, (data, target) in enumerate(train_loader):
         # Get Samples
         # data, target = Variable(data.to(device)), Variable(target.to(device))
@@ -75,7 +88,7 @@ while cpt != 3 and epoch < num_epochs:
 
         # Display
         if batch_idx % 10 == 0 or batch_idx % 10 == 1:
-            print('\r Train Epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.6f}\t Accuracy: {:.6f}'.format(
+            print('\nTrain Epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.6f}\t Accuracy: {:.6f}'.format(
                 epoch,
                 batch_idx * len(data),
                 len(train_loader.dataset),
@@ -87,23 +100,23 @@ while cpt != 3 and epoch < num_epochs:
     precision = precision_score(target, torch.max(pred, 1)[1].float(), average='micro')
     recall = recall_score(target, torch.max(pred, 1)[1].float(), average='micro')
 
-    print('F1: {} | Precision: {} | Recall: {} | Accuracy: {}'.format(
+    print('\nF1: {} | Precision: {} | Recall: {} | Accuracy: {}'.format(
         str(f1),
         str(precision),
         str(recall),
         str(accuracy)), end='')
     print("=> end of train")
 
-    f.write("Epoch " + str(epoch) + "  F1 : " + str(f1) + " | Precision : " + str(precision) + " | Recall : " + str(recall) + " | Accuracy : " + str(accuracy) + " | Loss : " + str(loss.data.item()))
-    f.write("\n=> end of train\n")
-
+    # with open(name, "a") as f:
+    append_new_line(name, "Epoch " + str(epoch) + "\n TRAIN =>  F1 : " + str(f1) + " | Precision : " + str(
+            precision) + " | Recall : " + str(
+            recall) + " | Accuracy : " + str(accuracy) + " | Loss : " + str(loss.data.item()))
     lossTmp = loss
 
     # Validation
     acc = 0
     tcn_model.eval()
     print("Start of validation")
-    f.write("Start of validation")
     with torch.no_grad():
         for batch_idx, (data, target) in enumerate(valid_loader):
             data, target = Variable(data), Variable(target)
@@ -120,7 +133,7 @@ while cpt != 3 and epoch < num_epochs:
             # Saving losses for display
             tcn_losses.append(loss.data.item())
             if batch_idx % 10 == 0 or batch_idx % 10 == 1:
-                print('\r Validation Epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.6f}'.format(
+                print('\nValidation Epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.6f}'.format(
                     epoch,
                     batch_idx * len(data),
                     len(valid_loader.dataset),
@@ -132,20 +145,21 @@ while cpt != 3 and epoch < num_epochs:
         recall = recall_score(target, torch.max(pred, 1)[1].float(), average='micro')
         accuracy = accuracy_score(target, torch.max(pred, 1)[1].float())
 
-        print('F1: {} | Precision: {} | Recall: {} | Accuracy: {}'.format(
+        print('\nF1: {} | Precision: {} | Recall: {} | Accuracy: {}'.format(
             str(f1),
             str(precision),
             str(recall),
             str(accuracy)), end='')
         print("=> end of validation")
 
-        f.write("Epoch " + str(epoch) + "  F1 : " + str(f1) + " | Precision : " + str(precision) + " | Recall : " + str(
-            recall) + " | Accuracy : " + str(accuracy) + " | Loss : " + str(loss.data.item()))
-        f.write(" => end of validation\n")
+        # with open(name, "a") as f:
+        append_new_line(name, "VALIDATION =>  F1 : " + str(f1) + " | Precision : " + str(precision) + " | Recall : " + str(
+                recall) + " | Accuracy : " + str(accuracy) + " | Loss : " + str(loss.data.item()) + "\n")
 
         if (loss < lossTmp):
             torch.save(tcn_model, "FinalModel" + str(datetime.datetime.now()) + ".pt")
             cpt = 0
         else:
             cpt += 1
-f.close()
+
+
